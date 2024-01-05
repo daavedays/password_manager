a2b0c81d7d52b0b0847cfe07fde33557ba137377
David Mirzoyan <151464384+daavedays@users.noreply.github.com>
2024-01-05 18:50:04 +0200

    main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ FONT = ("Ariel", 15)
 
 
 def password_generator():
+    """Function to generate passwords. Uses the alphabet, symbols and numbers and picks random characheters."""
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
